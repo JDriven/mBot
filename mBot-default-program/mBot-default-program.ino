ab68bb38fd89361335c64fca597b9f78827c7b2a
robin <robin.van.den.berg@jdriven.com>
2016-05-17 20:24:46 +0200

    Winning code! :)

--- mBot-default-program/mBot-default-program.ino
+++ mBot-default-program/mBot-default-program.ino
@@ -10,94 +10,148 @@ MeBuzzer buzzer;
 MeDCMotor MotorL(M1);
 MeDCMotor MotorR(M2);
 MePort generalDevice;
+MePort lightSensor(8);
+
+bool leftIn = false;
+bool rightIn = false;
 
 int defaultSpeed = 200;
 int minSpeed = 48;
 int maxSpeed = 250;
+double lightStrength;
 
-int moveSpeed = defaultSpeed;
+int moveSpeed = 150;// defaultSpeed
 
+int xCoord = 0;
+int yCoord = 0;
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Game On JDriven!");
   delay(1000);
+   rgb.setNumber(16);
+   //zingliedje();
+
 }
 
 void loop()
 {
-  // disco
-  disco();
-
-  // motor control
-  motorDemo();
-
-  // distance sensor
-  distanceDemo();
-
+ 
   // line follow sensor
-  lineFollowDemo();
+  readLeftAndRight();
+  followLine();
+  if (hasFinished()) {
+    // Maak muziekje.
+     halt();
+    zingliedje();
+    delay(1000*10);
+  }
+
 }
 
+bool hasFinished() {
+    lightStrength = lightSensor.aRead2();
+    Serial.print("LightStrength: ");
+    Serial.println(lightStrength);
+    return lightStrength < 750;
+ }
 
+void followLine() {
+  PrintLeftAndRight();
+  
+  if (leftIn && rightIn) {
+    forward();
+     rgb.setColor(0, 10, 0);
+  } else if (leftIn && !rightIn) {
+    // Hij gaat nu links eraf. ga naar rechts.
+    turnLeft();
+    rgb.setColor(10, 0, 0);
+    Serial.println("linksaf!");
+  } else if (!leftIn && rightIn) {
+    turnRight();
+    rgb.setColor(10, 0, 0);
+    Serial.println("rechtssaf!");
+  } else {
+    Serial.println("kruispunt!");
+    rgb.setColor(0, 0, 10);
+    // allebei op wit..
+    // FOOOOUT!
+    MotorL.run(0);
+    MotorR.run(0);
+    handleCrossing();
+ //     delay(3000);
 
-void  disco()
-{
-  rgb.setNumber(16);
-  rgb.clear();
-  rgb.setColor(10, 0, 0);
-  buzzer.tone(294, 300); 
-  delay(30);
-  rgb.setColor(0, 10, 0);
-  buzzer.tone(330, 300);
-  delay(30);
-  rgb.setColor(0, 0, 10);
-  buzzer.tone(350, 300);
-  delay(30);
-  rgb.clear();
-
-  buzzer.noTone();
+  }
 }
-       
-void motorDemo() 
-{
-  Serial.print("Move Speed: ");
-  Serial.println(moveSpeed);
-  
-  Serial.println("Forward!");
-  forward();
-  delay(1000);
-  
-  Serial.println("Backward!");
-  backward();
-  delay(1000);
-  
-  Serial.println("TurnLeft!");
-  turnLeft();
-  delay(1000);
-  
-  Serial.println("TurnRight!");
-  turnRight();
-  delay(1000);
 
-  Serial.println("ChangeSpeed to minSpeed and Forward");
-  changeSpeed(minSpeed);
+void handleCrossing() {
+  Serial.println("klein stukje doorrijden");
   forward();
-  delay(1000);
-
-  Serial.println("ChangeSpeed to maxSpeed and Forward");
-  changeSpeed(maxSpeed);
-  forward();
-  delay(1000);
-
-  Serial.println("ChangeSpeed to maxSpeed and Forward");
-  changeSpeed(maxSpeed);
-  forward();
-  delay(1000);
-
-  Serial.println("Halt");
+  delay(60000/moveSpeed);
   halt();
+  
+  Serial.println("we gaan eerst rechtsaf");
+  turnBy90degrees(-1);
+  while(true) {
+    if (isThereAWall()) {
+         Serial.println("Er is een muur, links met 90 graden");
+        buzzer.tone(294, 300);
+        turnBy90degrees(1);
+    } else {
+         Serial.println("We gaan weer verder!");
+
+      return;
+     // to followline
+    }
+  }
+  
 }
+
+
+void turnBy90degrees(int8_t leftOrRight) { 
+          
+  Serial.println("We beginnen met draaien");
+ 
+  // leftOrRight = -1 is rechts.
+  if (leftOrRight == 1) {
+    Serial.println("links");
+    MotorL.run(moveSpeed);
+    MotorR.run(moveSpeed);
+  } else {
+    Serial.println("rechts");
+    MotorL.run(-moveSpeed);
+    MotorR.run(-moveSpeed);
+  }
+  // delay(0.4*150000/moveSpeed);
+
+
+  Serial.println(S1_IN_S2_IN);
+  while(line.readSensors() == S1_IN_S2_IN) {
+    Serial.println(line.readSensors());
+    // Zolang nog op zwart
+    delay(1000/moveSpeed);
+  }
+  Serial.println("na eerste loop");
+  while(line.readSensors() != S1_IN_S2_IN) {
+        Serial.println(line.readSensors());
+    // Zolang nog op wit
+    delay(1000/moveSpeed);
+  }
+  Serial.println("na tweede loop");
+  
+  halt();
+  Serial.println("We stoppen met draaien");
+
+}
+
+bool isThereAWall() {
+    uint8_t d = ultr.distanceCm(50);
+    Serial.print("Afstand tot de muur gemeten: ");
+    Serial.print(d);
+    Serial.println();
+  return d < 29 && d !=0;
+}
+
 
 void forward()
 {
@@ -106,18 +160,18 @@ void forward()
 }
 void backward()
 {
-  MotorL.run(moveSpeed); 
+  MotorL.run(moveSpeed);
   MotorR.run(-moveSpeed);
 }
 void turnLeft()
 {
-  MotorL.run(-moveSpeed/10);
+  MotorL.run(-moveSpeed / 10);
   MotorR.run(moveSpeed);
 }
 void turnRight()
 {
   MotorL.run(-moveSpeed);
-  MotorR.run(moveSpeed/10);
+  MotorR.run(moveSpeed / 10);
 }
 void halt()
 {
@@ -126,27 +180,10 @@ void halt()
   MotorR.run(0);
 }
 
-void changeSpeed(int spd)
-{
-  buzzer.tone(441, 300); 
-  moveSpeed = spd;
-}
-
-
-void distanceDemo()
-{
-  uint8_t d = ultr.distanceCm(50);
-  Serial.print("Distance: ");
-  Serial.println(d);
-}
-
-void lineFollowDemo()
+void readLeftAndRight()
 {
   uint8_t val = line.readSensors();
 
-  bool leftIn = false;
-  bool rightIn = false;
-  
   switch (val)
   {
     case S1_IN_S2_IN:
@@ -169,10 +206,200 @@ void lineFollowDemo()
       rightIn = false;
       break;
   }
-  Serial.print("LeftIn: ");
-  Serial.println(leftIn);
-  Serial.print("RightIn: ");
-  Serial.println(rightIn);
+
+
+}
+
+void PrintLeftAndRight() {
+    Serial.print("LeftIn: ");
+  Serial.print(leftIn);
+  Serial.print("\tRightIn: ");
+  Serial.print(rightIn);
+  Serial.println();
+}
+
+
+
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+ 
+#define melodyPin 3
+//Mario main theme melody
+int melody[] = {
+  NOTE_E7, NOTE_E7, 0, NOTE_E7,
+  0, NOTE_C7, NOTE_E7, 0,
+  NOTE_G7, 0, 0,  0,
+  NOTE_G6, 0, 0, 0,
+ 
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+ 
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0,
+ 
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+ 
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0
+};
+//Mario main them tempo
+int tempo[] = {
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+};
+
+void zingliedje() {
+ Serial.println(" 'Mario Theme'");
+    int size = sizeof(melody) / sizeof(int);
+    for (int thisNote = 0; thisNote < size; thisNote++) {
+ 
+      // to calculate the note duration, take one second
+      // divided by the note type.
+      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      int noteDuration = 1000 / tempo[thisNote];
+ 
+      buzz(melodyPin, melody[thisNote], noteDuration);
+ 
+      // to distinguish the notes, set a minimum time between them.
+      // the note's duration + 30% seems to work well:
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+ 
+      // stop the tone playing:
+      buzz(melodyPin, 0, noteDuration);
+    }
+}
+      
+void buzz(int targetPin, long frequency, long length) {
+
+if (frequency == 0) {
+  return;
+}
+Serial.println(frequency);
+Serial.println(length);
+
+ buzzer.tone(frequency, length);
+  
+
 }
 
 
